bfc901d3ec238127985fa437967000845c81d139
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-02 22:35:37 -0300

    1 file modified

--- 0.1.2/index.js
+++ 0.1.2/index.js
@@ -1,20 +1,26 @@
-let valorCompra = 100;
-let isConvenio = false;
-let isCartao = true;
-
 const compraQuinzePorCento = (isConvenio, isCartao, valorCompra) =>
   isConvenio && isCartao ? valorCompra - valorCompra * 0.15 : valorCompra;
 
 const compraDezPorCento = (isConvenio, isCartao, valorCompra) =>
   (isConvenio && isCartao) || valorCompra - valorCompra * 0.1;
 
-if (
-  (isConvenio === true && isCartao === false) ||
-  (isConvenio === false && isCartao === true)
-) {
-  console.log(compraDezPorCento(isConvenio, isCartao, valorCompra));
-} else if (isCartao === true && isConvenio === true) {
-  console.log(compraQuinzePorCento(isConvenio, isCartao, valorCompra));
-} else {
-  console.log(valorCompra);
+function validadorDesconto(valorCompra, isConvenio, isCartao) {
+  if (
+    (isConvenio === true && isCartao === false) ||
+    (isConvenio === false && isCartao === true)
+  ) {
+    let descontoDez = compraDezPorCento(isConvenio, isCartao, valorCompra);
+    return descontoDez;
+  } else if (isCartao === true && isConvenio === true) {
+    let deconstoQuinze = compraQuinzePorCento(
+      isConvenio,
+      isCartao,
+      valorCompra
+    );
+    return deconstoQuinze;
+  } else {
+    return valorCompra;
+  }
 }
+
+console.log(validadorDesconto(100, false, false));

42a002edbb59aed932b0b3d6c4296879f99d997e
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-09 20:44:26 -0300

    1 file modified

--- Exercicios/0.1.5/index.js
+++ Exercicios/0.1.5/index.js
@@ -1,9 +1,4 @@
-function validadorDesconto(
-  valorCompra,
-  isConvenio,
-  isCartao,
-  isPrimeiraCompra
-) {
+function validadorDesconto(valorCompra,isConvenio,isCartao,isPrimeiraCompra) {
   const compraCincoPorCento = (valorCompra) => valorCompra - valorCompra * 0.05;
   const compraDezPorCento = (valorCompra) => valorCompra - valorCompra * 0.1;
   const compraQuinzePorCento = (valorCompra) =>
@@ -11,28 +6,28 @@ function validadorDesconto(
 
   let valorTotalDesconto;
   if (isPrimeiraCompra) {
-    valorTotalDesconto = compraCincoPorCento(valorCompra);
+    valorTotalDesconto = valorCompra * 0.05;
     if (valorTotalDesconto > 100) {
       return valorCompra - 100;
     } else {
-      return valorTotalDesconto;
+      return compraCincoPorCento(valorCompra);
     }
   } else if (isCartao && isConvenio && !isPrimeiraCompra) {
-    valorTotalDesconto = compraDezPorCento(valorCompra);
+    valorTotalDesconto = valorCompra * 0.15;
     if (valorTotalDesconto > 100) {
       return valorCompra - 100;
     } else {
-      return valorTotalDesconto;
+      return compraQuinzePorCento(valorCompra);
     }
   } else if (isCartao || (isConvenio && !isPrimeiraCompra)) {
-    valorTotalDesconto = compraQuinzePorCento(valorCompra);
+    valorTotalDesconto = valorCompra * 0.15;
     if (valorTotalDesconto > 100) {
       return valorCompra - 100;
     } else {
-      return valorTotalDesconto;
+      return compraDezPorCento(valorCompra);
     }
   } else {
     return valorCompra;
   }
 }
-console.log(validadorDesconto(800, false, false, true));
+console.log(validadorDesconto(800, true, true, false));

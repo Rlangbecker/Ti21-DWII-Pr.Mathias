31ffd71665d76aed060889de0385514e88f6f26a
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-09 19:47:08 -0300

    1 file modified

--- Exercicios/0.1.5/index.js
+++ Exercicios/0.1.5/index.js
@@ -4,17 +4,25 @@ function validadorDesconto(
   isCartao,
   isPrimeiraCompra
 ) {
-  const compraQuinzePorCento = (isConvenio, isCartao, valorCompra) =>
-    isConvenio && isCartao ? valorCompra - valorCompra * 0.15 : valorCompra;
-  const compraDezPorCento = (isConvenio, isCartao, valorCompra) =>
-    (isConvenio && isCartao) || valorCompra - valorCompra * 0.1;
-  if (isCartao && isConvenio && valorCompra < 100) {
+  const compraCincoPorCento = (isConvenio, isCartao,isPrimeiraCompra, valorCompra) =>valorCompra - valorCompra * 0.05;
+  const compraDezPorCento = (isConvenio, isCartao, valorCompra) =>valorCompra - valorCompra * 0.1;
+    const compraQuinzePorCento = (isConvenio, isCartao, valorCompra) => valorCompra - valorCompra * 0.15;
+  if (
+    isCartao &&
+    isConvenio &&
+    valorCompra < 100 &&
+    isPrimeiraCompra === false
+  ) {
     return compraDezPorCento(isConvenio, isCartao, valorCompra);
-  } else if (isCartao || (isConvenio && valorCompra < 100)) {
+  } else if (
+    isCartao ||
+    (isConvenio && valorCompra < 100 && isPrimeiraCompra === false)
+  ) {
     return compraQuinzePorCento(isConvenio, isCartao, valorCompra);
+  } else if(isPrimeiraCompra) {
+    return compraCincoPorCento;
   } else {
     return valorCompra;
-  }
 }
 
-console.log(validadorDesconto(100, true, true));
+console.log(validadorDesconto(100, true, true, false));

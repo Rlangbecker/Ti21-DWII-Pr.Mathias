d1e87b22614f6dd6a1047760dbfb64668aacbfda
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-09 21:42:31 -0300

    1 file added

---
+++ Aula2_09.09/Exemplo_objeto.js
@@ -0,0 +1,9 @@
+const pessoa = {};
+pessoa.nome = 'Ricardo';
+pessoa.vegetariano = false;
+
+const pessoa2 = {};
+pessoa2.nome = 'mathias';
+pessoa2.vegetariano = true;
+
+console.log(pessoa, pessoa2);

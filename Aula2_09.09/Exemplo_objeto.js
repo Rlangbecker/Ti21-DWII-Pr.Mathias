50b9763adef42937bbf7859ab6d450eb781e2a11
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-09 21:54:49 -0300

    1 file modified

--- Aula2_09.09/Exemplo_objeto.js
+++ Aula2_09.09/Exemplo_objeto.js
@@ -1,3 +1,5 @@
+//objeto mais simples (vazio) const objetoVazio = {};
+
 const pessoa = {};
 pessoa.nome = 'Ricardo';
 pessoa.vegetariano = false;
@@ -5,5 +7,8 @@ pessoa.vegetariano = false;
 const pessoa2 = {};
 pessoa2.nome = 'mathias';
 pessoa2.vegetariano = true;
+
+// nome e vegetariano são propriedades do objeto
+//objeto literal é quando as propriedades de um objeto PESSOA1 não coincide com propriedades da PESSOA2
 
 console.log(pessoa, pessoa2);

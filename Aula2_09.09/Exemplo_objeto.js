af9fca4147cf28ccd98d88793a5dd276460cdc78
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-09 22:17:24 -0300

    1 file modified

--- Aula2_09.09/Exemplo_objeto.js
+++ Aula2_09.09/Exemplo_objeto.js
@@ -4,6 +4,9 @@ const pessoa = {};
 pessoa.nome = 'Ricardo';
 pessoa.vegetariano = false;
 
+//verificação de propriedade
+// if(typeof pessoa.nome === 'string'){ console.log('é string') };
+
 const pessoa2 = {};
 pessoa2.nome = 'mathias';
 pessoa2.vegetariano = true;

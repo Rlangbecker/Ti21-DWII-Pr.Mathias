0c2d24b36d88eed7d5fee497546ed71c15cec6cf
Rlangbecker <101938476+Rlangbecker@users.noreply.github.com>
2022-09-21 20:32:46 -0300

    1 file modified

--- Exercicios/0.2.2/index.js
+++ Exercicios/0.2.2/index.js
@@ -1,30 +1,18 @@
 //Faça a modelagem e definição de um objeto que represente o usuário de um sistema. O sistema é um sistema de blog. O objeto deve ter dados básicos de cadastro do usuário e tambén deve conter dados relacionados a  comentários avaliativos de outros usuario.
 
-const usuario = {
-  dadosUsuario: {
+const usuario = [
+  {
     nome: 'Ricardo',
     dataNascimento: '07/03/1994',
     dataCadastro: '21/03/2022',
     sexo: 'Masculino',
+    comentarios: [
+      {
+        conteudo: 'hahahaha muito bom!',
+        deuLike: true,
+      },
+    ],
   },
-  comentarios: [
-    {
-      conteudo: 'hahahaha muito bom!',
-      deuLike: true,
-    },
-    {
-      conteudo: 'Não gostei!',
-      deuLike: false,
-    },
-    {
-      conteudo: 'May the force be with you',
-      deuLike: true,
-    },
-  ],
-};
+];
 
 console.log(usuario);
-
-console.log(usuario.comentarios[0]);
-console.log(usuario.comentarios[1]);
-console.log(usuario.comentarios[2]);
